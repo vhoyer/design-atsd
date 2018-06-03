10226c7ace41aae1ed8619e442a838b9f0901b67
Vinícius Hoyer <vhoyer@live.com>
2018-06-03 23:31:07 +0000

    first version

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
 $('#main-carousel').slick({
     accessibility: false,
-//    autoplay: true,
+    autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
     pauseOnHover: false,
+    pauseOnFocus: false,
+});
+
+$('#client-say').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
     pauseOnFocus: false,
 });
